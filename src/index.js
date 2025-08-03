8ffc383e43ad3dc0e3bdd17854fe0fa0adcf81a2
Silvana Marzano <sil@MacBook-Air-de-Silvana.local>
2025-08-02 22:42:34 -0300

    Modificación de index.js antes de aplicar el codemod

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Cambiado a 'react-dom/client' en React 18
 import App from './App';
 
+// Crear el contenedor root para React
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderizar la aplicación con React.StrictMode habilitado para detectar problemas
 root.render(
-  <App />  {/* Elimina <React.StrictMode> */}
+  <React.StrictMode>
+    <App />  // Esto activará las verificaciones adicionales para los componentes
+  </React.StrictMode>
 );
